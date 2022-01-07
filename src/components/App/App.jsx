8b358fa2ad5d5af392bd02f8a758e3299ecf8b89
Serhii Panasiuk <regidmail.sergej@gmail.com>
2022-01-08 00:27:42 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { GlobalStyle } from './GLobalStyle';
 import { Statistics, FeedbackOptions, Section, Notification } from 'components';
 
